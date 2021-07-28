7fa16a354fcb9ffc37486e6992cdf414b6feebf5
Breno Henrique <bhfs.2908@gmail.com>
2021-07-28 17:48:03 -0300

    feat:add footer component

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -4,5 +4,7 @@ import './Footer.css';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
     <footer className="footer">
-        Rodape
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Breno</strong> 
+        </span>
     </footer>
