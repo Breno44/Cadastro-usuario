6c5b660c592aa3259c37770fe6d6e45dfc991b19
Breno Henrique <bhfs.2908@gmail.com>
2021-07-28 16:48:34 -0300

    feat:logo component

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css';
+import Logo from '../../assets/imgs/Group 2.png';
 import React from 'react';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={Logo} alt="logo" />
+        </a>
     </aside>
