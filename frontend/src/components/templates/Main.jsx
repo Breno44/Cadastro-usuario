f24f36fead4556e8421b43ae9ae8a92890e07cde
Breno Henrique <bhfs.2908@gmail.com>
2021-07-28 17:32:45 -0300

    feat:add header component

--- frontend/src/components/templates/Main.jsx
+++ frontend/src/components/templates/Main.jsx
@@ -5,7 +5,7 @@ import Header from './Header';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
     <React.Fragment>
-        <Header />
+        <Header {...props} />
         <main className="content">
             conteudo
         </main>
